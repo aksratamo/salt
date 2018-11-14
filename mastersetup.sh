b22caccd3571d13e67a447d9f82bf13ba68b280e
Akseli Ratamo <ratamo.aks@gmail.com>
2018-11-14 15:20:04 +0200

    Update mastersetup.sh

--- mastersetup.sh
+++ mastersetup.sh
@@ -5,9 +5,9 @@ echo "Setting keyboard layout to fi-FI"
 setxkbmap fi
 cd ~
 echo "Updating packages..."
-apt-get update -qq >> /dev/null
+sudo apt-get update -qq >> /dev/null
 echo "Installing git and salt..."
-apt-get install git salt-master salt-minion -y -qq >> /dev/null
+sudo apt-get install git salt-master salt-minion -y -qq >> /dev/null
 echo "Cloning repository..."
 cd /srv/
 git clone https://github.com/aksratamo/salt
