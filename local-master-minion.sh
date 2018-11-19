fbfcfea24534686210b310a3e3acc4b04ca7eecd
Akseli Ratamo <ratamo.aks@gmail.com>
2018-11-19 19:17:37 +0200

    Create local-master-minion.sh

---
+++ local-master-minion.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#installs git and saltmaster&minion sets keyboard to fi
+
+echo "Setting keyboard layout to fi-FI"
+setxkbmap fi
+cd
+echo "Updating packages..."
+sudo apt-get update -qq >> /dev/null
+echo "Installing git and salt..."
+sudo apt-get install git salt-master salt-minion -y -qq >> /dev/null
+echo "Cloning repository..."
+cd /srv/
+sudo git clone https://github.com/aksratamo/salt
+
+echo "Writing salt settings to file and restarting salt-minion..."
+sudo echo -e "master: localhost\nid: localminion" | sudo tee /etc/salt/minion
+sudo systemctl restart salt-minion
+sudo salt-key -A on your master
