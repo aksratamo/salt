ff0c99c33827fe1f2d1ce88cea0dcc1a3ad180bc
Akseli Ratamo <ratamo.aks@gmail.com>
2018-11-19 19:06:48 +0200

    Update mastersetup.sh

--- mastersetup.sh
+++ mastersetup.sh
@@ -12,12 +12,12 @@ echo "Cloning repository..."
 cd /srv/
 sudo git clone https://github.com/aksratamo/salt
 echo "Adding mininon"
-apt-get install salt-minion -y -qq >> /dev/null
+sudo apt-get install salt-minion -y -qq >> /dev/null
 echo "Please enter master IP address here:"
 read MasterIP
 echo "Please enter a unique id for your system:"
 read SystemID
 echo "Writing salt settings to file and restarting salt-minion..."
-echo -e "master: $MasterIP\nid: ws-$SystemID" | sudo tee /etc/salt/minion
-systemctl restart salt-minion
+sudo echo -e "master: $MasterIP\nid: ws-$SystemID" | sudo tee /etc/salt/minion
+sudo systemctl restart salt-minion
 echo "Please run sudo salt keys -A on your master"
