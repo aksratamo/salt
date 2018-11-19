2b29834f716c305e7b9f9dd769d931e2c6ae94b0
Akseli Ratamo <ratamo.aks@gmail.com>
2018-11-19 19:58:34 +0200

    Update local-master-minion.sh

--- local-master-minion.sh
+++ local-master-minion.sh
@@ -3,9 +3,9 @@
 
 cd
 echo "Updating packages..."
-sudo apt update -qq >> /dev/null
+sudo apt-get update -qq >> /dev/null
 echo "Installing git and salt..."
-sudo apt install git salt-master salt-minion -y -qq >> /dev/null
+sudo apt-get install git salt-master salt-minion -y -qq >> /dev/null
 echo "Cloning repository..."
 cd /srv/
 sudo git clone https://github.com/aksratamo/salt
