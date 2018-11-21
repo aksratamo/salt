892838f4144e40269ae1bc6ba9167d022db79ad1
Akseli Ratamo <ratamo.aks@gmail.com>
2018-11-21 10:38:34 +0200

    firefox system wide settings started

---
+++ linux_apps/syspref.js
@@ -0,0 +1,5 @@
+//Disable title bar
+pref('browser.tabs.drawInTitlebar', true);
+pref('general.autoScroll', true);
+pref('lightweightThemes.selectedThemeID', 'firefox-compact-light@mozilla.org');
+pref('browser.tabs.warnOnClose', false);
