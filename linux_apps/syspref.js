5c65cfd29036e233c4d5044ca8243ab9784fe810
Akseli Ratamo <ratamo.aks@gmail.com>
2018-11-21 16:47:43 +0200

    Update syspref.js

--- linux_apps/syspref.js
+++ linux_apps/syspref.js
@@ -1,5 +1,12 @@
+//Firefox settings  file
+
 //Disable title bar
 pref('browser.tabs.drawInTitlebar', true);
 pref('general.autoScroll', true);
 pref('lightweightThemes.selectedThemeID', 'firefox-compact-light@mozilla.org');
 pref('browser.tabs.warnOnClose', false);
+
+// scrolling
+pref('general.smoothScroll.mouseWheel.durationMaxMS', 600);
+pref('general.smoothScroll.mouseWheel.durationMinMS', 400);
+pref('mousewheel.min_line_scroll_amount', 80);
