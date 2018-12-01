d51bcf07a6a1ceaa5d0441170d47af1ed90e6fe0
Akseli Ratamo <a1602642@myy.haaga-helia.fi>
2018-12-01 11:13:44 +0000

    Made local instalation easier with masterless call and added linux-advanced with ppa

---
+++ linux-advanced.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Installs top.sls and advanced-linux-apps
+cd
+echo "Updating packages..."
+sudo apt-get update -qq >> /dev/null
+echo "Installing git and salt..."
+sudo apt-get install git salt-master salt-minion -y -qq >> /dev/null
+echo "Cloning repository..."
+cd /srv/
+sudo git clone https://github.com/aksratamo/salt
+
+#Running highstate and advanced-linux
+sudo salt-call --local state.apply
+sudo salt-call --local state.apply
---
+++ linux-advanced/init.sls
@@ -0,0 +1,11 @@
+nextcloud-ppa:
+  pkgrepo.managed:
+    - ppa: nextcloud-devs/client
+
+
+linux-advanced:
+  pkg.installed:
+    - pkgs:
+      - neofetch
+      - keepassxc
+
