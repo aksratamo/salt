1659923cdbc5268f3a3b3f892aff2906c151faa6
Akseli Ratamo <ratamo.aks@gmail.com>
2018-12-01 22:03:44 +0200

    testing salt curl installer

--- instalation-scripts/linux-advanced.sh
+++ instalation-scripts/linux-advanced.sh
@@ -4,7 +4,9 @@ cd
 echo "Updating packages..."
 sudo apt-get update -qq >> /dev/null
 echo "Installing git and salt..."
-sudo apt-get install git salt-minion -y -qq >> /dev/null
+sudo apt-get install git -y -qq >> /dev/null
+curl -L https://bootstrap.saltstack.com -o install_salt.sh
+sudo sh install_salt.sh -P
 echo "Cloning repository..."
 cd /srv/
 sudo git clone https://github.com/aksratamo/salt
