f1cc2696364f26538d3229308700fb1614c59cbe
Akseli Ratamo <ratamo.aks@gmail.com>
2018-12-04 10:23:18 +0000

    Update syspref.js

--- linux-apps/syspref.js
+++ linux-apps/syspref.js
@@ -30,8 +30,4 @@ pref('toolkit.telemetry.updatePing.enabled', false);
 
 
 pref('browser.startup.homepage', 'https://duckduckgo.com/');
-<<<<<<< HEAD
 
-
-=======
->>>>>>> fa7a6d65470b60c1574bb84e6d2ff2ec5ec5a5f1
