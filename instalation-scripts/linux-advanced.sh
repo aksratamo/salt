db68d57e3ad3e516a19ea7bee4fbe87e90b96d2f
Akseli Ratamo <ratamo.aks@gmail.com>
2018-12-08 12:57:03 +0200

    Update linux-advanced.sh

--- instalation-scripts/linux-advanced.sh
+++ instalation-scripts/linux-advanced.sh
@@ -11,6 +11,7 @@ sudo apt-get update -qq >> /dev/null
 echo "===> Installing git and salt... <==="
 sudo apt-get install git curl salt-minion -y 
 if [ ! -d /srv/]; then
+echo "===> Installing  salt-minion with workaround bootstrap instalation... <==="
 curl -L https://bootstrap.saltstack.com -o install_salt.sh
 sudo sh install_salt.sh -P 
 fi
