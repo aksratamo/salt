c41d0ea8dacd0de4ad9bb49b599f6dc1cfc52ea1
Akseli Ratamo <ratamo.aks@gmail.com>
2018-12-08 14:20:46 +0200

    Update linux-advanced.sh

--- instalation-scripts/linux-advanced.sh
+++ instalation-scripts/linux-advanced.sh
@@ -9,9 +9,13 @@ wget -O - https://repo.saltstack.com/apt/debian/9/amd64/latest/SALTSTACK-GPG-KEY
 cd /etc/apt/sources.list.d/
 wget https://raw.githubusercontent.com/aksratamo/salt/master/saltstack.list
 cd
+echo "="
 echo "===> Updating packages... <==="
+echo "="
 sudo apt-get update 
+echo "="
 echo "===> Installing git and salt... <==="
+echo "="
 sudo apt-get install git curl salt-minion -y 
 #if [ ! -d /srv/]; then
 #echo "===> Installing  salt-minion with workaround bootstrap instalation... <==="
@@ -19,13 +23,16 @@ sudo apt-get install git curl salt-minion -y
 #sudo sh install_salt.sh -P 
 #fi
 sudo echo -e "master: localhost\nid: linux-advanced" | sudo tee /etc/salt/minion
+echo "="
 echo "===> Cloning repository... <==="
+echo "="
 cd /srv/
 sudo git clone https://github.com/aksratamo/salt
 cd salt/
 # sudo wget https://someonewhocares.org/hosts/hosts
 
-
+echo "="
 echo "===> Running highstate and advanced-linux, Please wait <==="
+echo "="
 sudo salt-call --local state.apply 
 # --state-output=terse
