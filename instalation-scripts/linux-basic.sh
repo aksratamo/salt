b8c870f4e0ef08bff3aa8187e465dced2ef7d7d5
Akseli Ratamo <ratamo.aks@gmail.com>
2019-04-02 09:58:40 +0300

    Update linux-basic.sh

--- instalation-scripts/linux-basic.sh
+++ instalation-scripts/linux-basic.sh
@@ -6,7 +6,7 @@ if [ -f /tmp/helloworld.txt ]; then
     echo "===> This install script has already been run! It is intended to be run only once <==="
     exit 0
 fi
-wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+sudo wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 echo "deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main" | sudo tee /etc/apt/sources.list.d/saltstack.list
 echo "="
 echo "===> Updating packages... <==="
