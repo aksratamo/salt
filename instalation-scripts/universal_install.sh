193c2ec8aa55a6f2dd83c70b7f69a587dfd12c08
Akseli Ratamo <ratamo.aks@gmail.com>
2019-04-23 16:06:47 +0300

    adding test run option
    When setting $Systemflavour to "test" it will not run upgrade or state.apply for testing reasons. Does only work with ubuntu for now

--- instalation-scripts/universal_install.sh
+++ instalation-scripts/universal_install.sh
@@ -20,14 +20,25 @@ if [ ! -f /etc/salt/minion ]; then
 	    	echo "===> First run installing salt <==="
 	   	sudo wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 		echo "deb [arch=amd64] http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main" | sudo tee /etc/apt/sources.list.d/saltstack.list
-		echo "="
-		echo "===> Updating packages... <==="
-		echo "="
-		sudo apt-get update && sudo apt upgrade -y
-		echo "="
-		echo "===> Installing git and salt... <==="
-		echo "="
-		sudo apt-get install git salt-minion -y 
+		
+		if [ $Systemflavour == test ]; then
+
+			echo "="
+			echo "===> Test run, not updating <==="
+			echo "="
+
+		else
+			echo "="
+			echo "===> Updating packages... <==="
+			echo "="
+			sudo apt-get update && sudo apt upgrade -y
+		fi		
+
+			echo "="
+			echo "===> Installing git and salt... <==="
+			echo "="
+			sudo apt-get install git salt-minion -y 
+		
 
 	fi
 	echo "="
@@ -43,6 +54,16 @@ fi
     	echo "===> Just changing Minion ID <==="
 	echo -e "master: localhost\nid: linux-$MinionID" | sudo tee /etc/salt/minion
 	echo "="
-	echo "===> Running highstate with MinionID ¤ $MinionID ¤ , Please wait <==="
-	echo "="
-	sudo salt-call --local state.apply 
+	
+		if [ $Systemflavour == test ]; then
+
+			echo "="
+			echo "===> Test run, not running salt <==="
+			echo "="
+
+		else
+			echo "="
+			echo "===> Running highstate with MinionID ¤ $MinionID ¤ , Please wait <==="
+			echo "="
+			sudo salt-call --local state.apply 
+		fi		
