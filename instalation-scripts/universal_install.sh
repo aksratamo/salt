f1668de6d2b0885bef5974df23c760f0f43294af
Akseli Ratamo <ratamo.aks@gmail.com>
2019-04-24 14:37:38 +0300

    automatic OS identification, new test mode

--- instalation-scripts/universal_install.sh
+++ instalation-scripts/universal_install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # Installs top.sls and advanced-linux-apps
 # Check if script has been run before
-
-echo "Set to basic or advanced:"
+if [ "$1" == test ]; then
+        echo "===> TEST RUN <===" 
+fi
+echo "Set to basic or advanced. Add gaming for Lutris"
 read MinionID
 if [ ! -f /etc/salt/minion ]; then
-
-	echo "set ubuntu or arch based on what you are running:"
-	read Systemflavour
-	
-	if [ $Systemflavour == arch ]; then
+	if [ "$(lsb_release -is)" == ManjaroLinux ] || [ "$(lsb_release -is)" == Arch ]; then
 		sudo pacman -Syu -y --needed
 		echo "="
 		echo "===> Installing git and salt... <==="
@@ -17,48 +15,49 @@ if [ ! -f /etc/salt/minion ]; then
 		sudo pacman -S git salt -y 
 
 	else
-	    	echo "===> First run installing salt <==="
-	   	sudo wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-		echo "deb [arch=amd64] http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main" | sudo tee /etc/apt/sources.list.d/saltstack.list
-		
-		if [ $Systemflavour == test ]; then
-
-			echo "="
-			echo "===> Test run, not running updates <==="
-			echo "="
-			sudo apt-get update
-		else
-			echo "="
-			echo "===> Updating packages... <==="
-			echo "="
-			sudo apt-get update && sudo apt upgrade -y
-		fi		
-
-			echo "="
-			echo "===> Installing salt-minion and git... <==="
-			echo "="
-			sudo apt-get install git salt-minion -y 
-		
-
+	        if [ "$(lsb_release -is)" == Ubuntu ]; then
+        	    	echo "===> First run installing salt <==="
+        	   	sudo wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+        		echo "deb [arch=amd64] http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main" | sudo tee /etc/apt/sources.list.d/saltstack.list
+        		
+        		if [ "$1" == test ]; then
+        
+        			echo "="
+        			echo "===> Test run, not running updates <==="
+        			echo "="
+        			sudo apt-get update
+        		else
+        			echo "="
+        			echo "===> Updating packages... <==="
+        			echo "="
+        			sudo apt-get update && sudo apt upgrade -y
+        		fi	
+        			echo "="
+        			echo "===> Installing salt-minion and git... <==="
+        			echo "="
+        			sudo apt-get install git salt-minion -y 
+                        else 
+                                echo "un supported os"
+                        fi 
 	fi
 	echo "="
 	echo "===> Cloning repository... <==="
 	echo "="
 	cd /srv/
 	sudo git clone https://github.com/aksratamo/salt
-	cd salt/
+	cd 
 
 
 fi
 
-    	echo "===> Just changing Minion ID <==="
+    	echo "===> Setting MinionID to ¤ $MinionID ¤  <==="
 	echo -e "master: localhost\nid: linux-$MinionID" | sudo tee /etc/salt/minion
 	echo "="
 	
-		if [ $Systemflavour == test ]; then
+		if [ "$1" == test ]; then
 
 			echo "="
-			echo "===> Test run, not running salt <==="
+			echo "===> Test run, making no changes <==="
 			echo "="
 			sudo salt-call --local state.apply test=True
 
@@ -67,4 +66,4 @@ fi
 			echo "===> Running highstate with MinionID ¤ $MinionID ¤ , Please wait <==="
 			echo "="
 			sudo salt-call --local state.apply 
-		fi		
+		fi
