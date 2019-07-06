4de704ef26acbe490a8d8c104cccaa04831f74b5
Akseli Ratamo <ratamo.aks@gmail.com>
2019-07-06 10:22:04 +0300

    added echo output color

--- instalation-scripts/universal_install.sh
+++ instalation-scripts/universal_install.sh
@@ -14,28 +14,32 @@ read MinionID
 # If Salt minion info does not exist install salt othervise skip salt installation
 if [ ! -f /etc/salt/minion ]; then
 
+#echo -e "\e[42mGREEN BACKGROUND COLOR TEXT HERE\e[0m"
+
+#echo -e "\e[42mGREEN BACKGROUND COLOR TEXT HERE\e[0m"
 
 
 # check if Arch or Ubuntu based
 # If Arch based install salt and git with pacman
 	if [ "$(lsb_release -is)" == ManjaroLinux ] || [ "$(lsb_release -is)" == Arch ]; then
 		sudo pacman -Syu -y --needed
-		echo "===> Installing git and salt... <==="
+		echo -e "\e[42mInstalling Salt on Arch based Linux\e[0m"
 		sudo pacman -S git salt -y 
 
 
 # If Ubuntu based but not Ubuntu lts based add Salt PPA (without PPA Salt is broken in non LTS)
 	elif [ "$(lsb_release -is)" == Ubuntu ] && [ "$(lsb_release -rs)" != 18.04 ] && [ "$(lsb_release -rs)" != 20.04 ] ; then
-       	    	echo "===> First run. Installing Salt with ppa.<==="
+       	    	echo -e "\e[42mInstalling Salt on Ubuntu based Linux with Salt PPA\e[0m"
        	   	sudo wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
        		echo "deb [arch=amd64] http://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest bionic main" | sudo tee /etc/apt/sources.list.d/saltstack.list
 		
 # If Ubuntu is LTS dont install Salt PPA
 	elif [ "$(lsb_release -is)" == Ubuntu ] && [ "$(lsb_release -rs)" == 18.04 ] || [ "$(lsb_release -rs)" == 20.04 ] ; then
-		echo "===> First run. Installing Salt. OS is LTS, salt ppa skipped <==="
+		echo -e "\e[42mInstalling Salt on Ubuntu LTS based Linux without Salt PPA\e[0m"
 
         else 
-                echo "Unsupported OS. ABORT"
+		echo -e "\e[41mYou seem to be running $(lsb_release -is) based Linux. Its not supported\e[0m"
+                echo -e "\e[41mEXIT\e[0m"
 		exit
 fi       	
 
@@ -48,13 +52,13 @@ fi
 # If ubuntu based install salt and git with apt
 	if [ "$(lsb_release -is)" == Ubuntu ]; then
 	       	if [ "$1" == test ]; then
-        		echo "===> Test run, not running updates <==="
+			echo -e "\e[43mTest run, not running updates\e[0m"
         		sudo apt-get update
         	else
-        		echo "===> Updating packages... <==="
+        		echo -e "\e[42mUpdateing packages\e[0m"
         		sudo apt-get update && sudo apt upgrade -y
         	fi	
-        		echo "===> Installing salt-minion and git... <==="
+			echo -e "\e[42mInstalling salt-minion and git\e[0m"
         		sudo apt-get install git salt-minion -y 
 
                         
@@ -78,20 +82,17 @@ fi
 
 
 # Add Salt minion info
-    	echo "===> Setting MinionID to ¤ $MinionID ¤  <==="
 	echo -e "master: localhost\nid: linux-$MinionID\nfile_client: local" | sudo tee /etc/salt/minion
 	
 		if [ "$1" == test ]; then
 
-			echo "="
-			echo "===> Test run, making no changes <==="
-			echo "="
+			echo -e "\e[43mTest run, making no changes\e[0m"
 			sudo salt-call --local state.apply test=True
 			sudo systemctl status salt-minion.service
 
 		else
+			echo -e "\e[42mRunning highstate with MinionID $MinionID Please wait\e[0m"
 			echo "="
 			echo "===> Running highstate with MinionID ¤ $MinionID ¤ , Please wait <==="
-			echo "="
 			sudo salt-call --local state.apply 
 fi
